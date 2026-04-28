400141166f821f83948dda24efff2b2428a969e8
besayo-atgit <b2029besayo@pshs.edu.ph>
2026-04-28 18:32:12 +0800

    hi sir roy

---
+++ public/4-GE1/1gjs.js
@@ -0,0 +1,51 @@
+let rating = 0;
+document.querySelectorAll(".star").forEach(star => {
+    star.addEventListener("click", function() {
+        rating = this.getAttribute("data-value");
+        updateRating(rating)
+    });
+});
+
+function updateRating(starsValue) {
+    document.querySelectorAll(".star").forEach(star => {
+        star.classList.remove("selected");
+        if (star.getAttribute("data-value") <= starsValue) {
+            star.classList.add("selected");
+        }
+    })
+}
+
+function addMovie() {
+    event.preventDefault();
+    const form = document.querySelector('form');
+    const data = new FormData(form);
+
+    const obj = Object.fromEntries(data.entries());
+    obj.rating = rating;
+
+    let objList = JSON.parse(localStorage.getItem("movies")) || [];
+    objList.push(obj)
+    localStorage.setItem("movies", JSON.stringify(objList));
+    console.log(obj);
+
+    displayMovies();
+    form.reset();
+    updateRating(0);
+    rating = 0;
+}
+
+function displayMovies() {   
+    const listValue = document.getElementById("movieList");
+    const movies = JSON.parse(localStorage.getItem("movies")) || [];
+
+    listValue.innerHTML = "";
+    movies.forEach((movie) => {
+        const li = document.createElement("li");
+        li.innerHTML = `
+            ${movie.title}, released on ${movie.yr} is a ${movie.genre} movie. Rating: ${"★".repeat(movie.rating)}
+        `
+        listValue.appendChild(li);
+    })
+}
+
+window.onload = displayMovies();
